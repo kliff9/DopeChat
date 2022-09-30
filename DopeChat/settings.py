0d56906d12db4d73e421492d6b213197485f6ed6
kliff <c.thelusca@hotmail.com>
2022-09-30 10:29:30 -0400

    update

--- DopeChat/settings.py
+++ DopeChat/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 STATIC_URL = 'static/'
@@ -129,9 +128,13 @@ STATIC_URL = 'static/'
 #     BASE_DIR / 'static'
 # ]
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
